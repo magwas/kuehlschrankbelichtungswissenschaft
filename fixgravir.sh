ecd922584163778407b856164800a0983b760a26
Arpad Magosanyi <m4gw4s@gmail.com>
2026-01-25 16:27:14 +0100

    new version of box

--- fixgravir.sh
+++ fixgravir.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat gravir.gcode |sed 's/G0 Z6.000/M5/;s/G0 Z4.000/M3 P100/' >gravir-fixed.gcode
+cat gravir.gcode |sed 's/G0 Z26.000/M5/;s/G0 Z24.000/M3 P100/;s/Z21.000//' >gravir-fixed.gcode
